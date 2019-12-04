92162ef74b7e300d2dca4e5a18428087b343c44b
Persez <690421912@qq.com>
2019-12-04 11:34:04 +0100

    Perse first Commit

--- KING_OF_XIANGQI/Program.cs
+++ KING_OF_XIANGQI/Program.cs
@@ -2,6 +2,49 @@
 
 namespace KING_OF_XIANGQI
 {
+    public abstract class Chess
+    {
+        int[,] location;
+        string color;
+        string name;
+        Boolean dead;
+        public void MovingRules()
+        {
+
+        }
+        public void Initialization()
+        {
+
+        }
+    }
+    class General : Chess
+    {
+
+    }
+    class Rook : Chess
+    {
+
+    }
+    class Horse : Chess
+    {
+
+    }
+    class Elephant : Chess
+    {
+
+    }
+    class Mandarin : Chess
+    {
+
+    }
+    class Cannon : Chess
+    {
+
+    }
+    class Pawn : Chess
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
