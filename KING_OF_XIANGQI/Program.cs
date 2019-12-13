8c4bf039dcc10c5ad99c9d8ca9cdc0780f068050
Mike-7777777 <mike467901371@yahoo.com>
2019-12-13 09:06:34 +0100

    906

--- KING_OF_XIANGQI/Program.cs
+++ KING_OF_XIANGQI/Program.cs
@@ -14,7 +14,7 @@ namespace KING_OF_XIANGQI
             //Console.WriteLine("111展示棋盘（带棋子）");
             a.InitialBoardForDisplay();
             dataTable.initColor();
-
+            int[] num = new int[2];
             //选择棋子 以 及 展示可以行走的棋子的possible movement（变色）
             // datatable 为 table 类型的初始棋盘
 
@@ -25,17 +25,7 @@ namespace KING_OF_XIANGQI
             {
                 dataTable.initColor();
                 Console.WriteLine("Red first. Please Choose a piece in coordinate");   //你好，红先黑后，请红方选择棋子。
-                string spot = Console.ReadLine();
-
-                //用标点分开
-                string[] spotarr = spot.Split(',');
-
-                //输出并转化为int数组
-                int[] num = new int[spotarr.Length];
-                for (int i = 0; i < spotarr.Length; i++)
-                {
-                    num[i] = int.Parse(spotarr[i]);
-                }
+                num = getRead();
                 int redx = num[0];
                 int redy = num[1];
                 var red1 = new Tuple<int, int>(redx, redy);
@@ -51,9 +41,9 @@ namespace KING_OF_XIANGQI
                 a.PossibleMovementPoint(dataTable);//Console.WriteLine("222选择棋子 以 及 展示可以行走的棋子的possible movement（变色）");
 
                 Console.WriteLine("Please enter the coordinate that you want to go");
-
-                int redx1 = Convert.ToInt32(Console.ReadLine());
-                int redy1 = Convert.ToInt32(Console.ReadLine());
+                num = getRead();
+                int redx1 = num[0];
+                int redy1 = num[1];
                 var red2 = new Tuple<int, int>(redx1, redy1);//接收棋子目的地的坐标
                 control.MoveP(red1, red2, dataTable.getArr());//调用View的possiblemove方法 //运行Controller-Move方法
                 a.PositionChangingDisplay(dataTable);//Console.WriteLine("333棋子走动的位置改变"); 
@@ -61,17 +51,7 @@ namespace KING_OF_XIANGQI
                                                      //black
                 dataTable.initColor();
                 Console.WriteLine("Black now. Please Choose a piece in coordinate");   //黑方选择棋子。
-                string spot1 = Console.ReadLine();
-
-                //用标点分开
-                string[] spotarr1 = spot.Split(',');
-
-                //输出并转化为int数组
-                int[] num1 = new int[spotarr.Length];
-                for (int i = 0; i < spotarr.Length; i++)
-                {
-                    num[i] = int.Parse(spotarr[i]);
-                }
+                num = getRead();
                 int blackx = num[0];
                 int blacky = num[1];
                 var black1 = new Tuple<int, int>(blackx, blacky);
@@ -87,13 +67,29 @@ namespace KING_OF_XIANGQI
                 a.PossibleMovementPoint(dataTable);//Console.WriteLine("222选择棋子 以 及 展示可以行走的棋子的possible movement（变色）");
 
                 Console.WriteLine("Please enter the coordinate that you want to go");
-                int blackx1 = Convert.ToInt32(Console.ReadLine());
-                int blacky1 = Convert.ToInt32(Console.ReadLine());
+                num = getRead();
+                int blackx1 = num[0];
+                int blacky1 = num[1];
                 var black2 = new Tuple<int, int>(blackx1, blacky1);//接收棋子目的地的坐标
                 control.MoveP(black1, black2, dataTable.getArr());//调用View的possiblemove方法 //运行Controller-Move方法
                 a.PositionChangingDisplay(dataTable);//Console.WriteLine("333棋子走动的位置改变"); 
             }
             
+        }
+        public static int[] getRead()
+        {
+            string spot = Console.ReadLine();
+
+            //用标点分开
+            string[] spotarr = spot.Split(',');
+
+            //输出并转化为int数组
+            int[] num = new int[spotarr.Length];
+            for (int i = 0; i < spotarr.Length; i++)
+            {
+                num[i] = int.Parse(spotarr[i]);
+            }
+            return num;
         }
     }
 }
