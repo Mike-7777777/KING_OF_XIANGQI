006868d73cef480f826acefe77179eebe9d6114a
Persez <49867763+Persez@users.noreply.github.com>
2019-12-30 23:59:21 +0100

    修改了报错信息
    修改了报错信息

--- KING_OF_XIANGQI/Program.cs
+++ KING_OF_XIANGQI/Program.cs
@@ -27,44 +27,43 @@ namespace KING_OF_XIANGQI
             bool checkwin = false;
             if (checkwin == false)
             {
-                table.InitColor();
-                Console.WriteLine("It's " + color + "'s round, please Choose a piece in coordinate（ex. 1,2 for left cornor horse）");
-                int[] locationNum = view.GetRead(); //store location in int[2].
-                int x = locationNum[0];
-                int y = locationNum[1];
-                var departure = new Tuple<int, int>(x, y); //store x,y together into a tuple.
-                if (table.arr[x, y] == null)
+               try
                 {
-                    Console.WriteLine("no piece in the position");
+                    table.InitColor();
+                    Console.WriteLine("It's " + color + "'s round, please Choose a piece in coordinate（ex. 1,2 for left cornor horse）");
+                    int[] locationNum = view.GetRead(); //store location in int[2].
+                    int x = locationNum[0];
+                    int y = locationNum[1];
+                    var departure = new Tuple<int, int>(x, y); //store x,y together into a tuple.
+                    controller.ChooseP(x, y, color);    //controller choose one pice to move.
+                    view.PossibleMovementPoint(table);  //view make the color change.
                     
-                    Play(controller,table, view, color);
-                }                
-                else
-                {
-                controller.ChooseP(x, y, color);    //controller choose one pice to move.
-                view.PossibleMovementPoint(table);  //view make the color change.
-                Console.WriteLine("Please enter the coordinate that you want to go");
-                locationNum = view.GetRead();
-                int x1 = locationNum[0];
-                int y1 = locationNum[1];
-                var destination = new Tuple<int, int>(x1, y1);  //store destination coordintes with Tuple.
-                int[,] colorTable = table.GetColor();
-                    if (colorTable[x1, y1] == 0)
-                    {
-                         Console.WriteLine("wrong place");
-                    
-                         Play(controller,table,view,color);
-                    }                
-                    else
-                    {
-                        controller.MoveP(departure, destination);//调用View的possiblemove方法 //运行Controller-Move方法
-                        view.PositionChangingDisplay(table); //Console.WriteLine("333棋子走动的位置改变"); 
-                        if ((table.GetPiece(x, y) is General && table.GetPiece(x1, y1) is General))
+                    L1: Console.WriteLine("Please enter the coordinate that you want to go");                       
+                        locationNum = view.GetRead();
+                        int x1 = locationNum[0];
+                        int y1 = locationNum[1];
+                        var destination = new Tuple<int, int>(x1, y1);  //store destination coordintes with Tuple.
+                        int[,] colorTable = table.GetColor();
+                        if (colorTable[x1, y1] == 0) // selected piece is not a vaild move.
                         {
-                        Console.WriteLine("game is over!");
-                        checkwin = true;
+                            Console.WriteLine("wrong place"); 
+                            goto L1; //display the wrong message and jump back to the selection of the user.
                         }
-                    }
+                        else
+                        {
+                            controller.MoveP(departure, destination);//调用View的possiblemove方法 //运行Controller-Move方法
+                            view.PositionChangingDisplay(table); //Console.WriteLine("333棋子走动的位置改变"); 
+                            if ((table.GetPiece(x, y) is General && table.GetPiece(x1, y1) is General)) // selected piece is general. 
+                            {
+                                Console.WriteLine("game is over!");
+                                checkwin = true; // make the boolean checkwin to true and end the if loop.
+                            }
+                        }
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("err:null");
+                    Play(controller, table, view, color);
                 }
             }
             return checkwin;
